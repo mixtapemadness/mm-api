66ba598bef8df3addc74f38de972b895b6b34cd0
RezikoChkadua <reziko1010@gmail.com>
2018-10-05 17:49:22 +0400

    fix: mailchimp repo

--- src/modules/mailchimp/mailchimpRepository.js
+++ src/modules/mailchimp/mailchimpRepository.js
@@ -1,5 +1,5 @@
 'use strict'
-const request = require('request-promise-native')
+const request = require('superagent')
 const MAILCHIMP_API_KEY = '1653629bf3451d835462f6246ee7082b-us18'
 const MAILCHIMP_INSTANCE = 'us18'
 const MAILCHIMP_LIST_ID = '167380d875'
@@ -10,7 +10,6 @@ class MailChimpRepository {
   }
 
   async Subscribe(args) {
-    console.log('args', args)
     return new Promise((resolve, reject) => {
       const Item = {
         ...args
@@ -19,22 +18,22 @@ class MailChimpRepository {
         email_address: args.email_address,
         status: 'subscribed'
       }
-      const options = {
-        json: data,
-        headers: {
-          Authorization: 'Basic ' + new Buffer('any:' + MAILCHIMP_API_KEY).toString('base64')
-        },
-        method: 'POST',
-        uri: `https://${MAILCHIMP_INSTANCE}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members/`
 
-      }
-      request(options)
-        .then(data => {
+      const url = `https://${MAILCHIMP_INSTANCE}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST_ID}/members/`
+
+
+      request.post(url)
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + MAILCHIMP_API_KEY).toString('base64'))
+        .send(data)
+        .end((err, response) => {
+          if (err) {
+            return reject(err)
+          }
+
           return resolve(Item)
         })
-        .catch(err => {
-          return reject(err)
-        })
+
     })
   }
 

2642db650546e12be1530c4694a11f56db3608cd
RezikoChkadua <reziko1010@gmail.com>
2018-10-10 11:21:01 +0400

    fix:fix cors errors

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ global.app = app
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization')
   res.header('Access-Control-Allow-Headers', '*')

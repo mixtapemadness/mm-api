83cff9dbdc0eb74db7f1fdb0ba2aa80ac2a0ae99
RezikoChkadua <reziko1010@gmail.com>
2018-10-10 11:24:54 +0400

    fix: cors errors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization')
-  res.header('Access-Control-Allow-Headers', '*')
+  // res.header('Access-Control-Allow-Headers', '*')
   if (req.method === 'OPTIONS') {
     res.sendStatus(200)
   } else {

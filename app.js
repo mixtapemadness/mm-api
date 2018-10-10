cada7d14e3950dd4d97302f483e55926b56efe2c
RezikoChkadua <reziko1010@gmail.com>
2018-10-10 11:47:47 +0400

    fix:cors errors on firefox

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ global.app = app
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  // res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization')
-  // res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization')
+  res.header('Access-Control-Allow-Headers', '*')
   if (req.method === 'OPTIONS') {
     res.sendStatus(200)
   } else {

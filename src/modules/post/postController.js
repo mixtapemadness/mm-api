d9c98ef6a03521bd88d711c1e1c1b7a44cee01bd
reziko <reziko1010@gmail.com>
2018-11-08 17:34:58 +0400

    fix:react share

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,7 +1,6 @@
 /* eslint max-len: 0 */
 /* eslint standard/object-curly-even-spacing: 0 */
 var config = require('app/config')
-var striptags = require('striptags')
 
 class PostController {
   constructor(wp) {
@@ -12,11 +11,12 @@ class PostController {
     const slug = req.params.slug
     const category = req.params.category
     try {
+      var noHTML = /(<([^>]+)>)/ig
       const post = await this.wp.posts().param('slug', slug)
       const media = await this.wp.media().id(post[0].featured_media)
-      const title = post[0].title.rendered
+      const title = post[0].title.rendered.replace(noHTML, '')
       const img = media.media_details.sizes['featured-image'].source_url
-      const description = striptags(post[0].excerpt.rendered)
+      const description = post[0].excerpt.rendered.replace(noHTML, '')
 
       res.render('postShare', { url: config.HTTP_HOST, frontUrl: config.front_url, description, title, img, category, slug})
     } catch (e) {

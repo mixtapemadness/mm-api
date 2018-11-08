497fbdc95bc2efd00b50c846b1e7d19f25278ccd
reziko <reziko1010@gmail.com>
2018-11-08 16:41:22 +0400

    fix:strip html on costum react share

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,6 +1,7 @@
 /* eslint max-len: 0 */
 /* eslint standard/object-curly-even-spacing: 0 */
 var config = require('app/config')
+const stripHtml = require('string-strip-html')
 
 class PostController {
   constructor(wp) {
@@ -13,7 +14,8 @@ class PostController {
     try {
       const post = await this.wp.posts().param('slug', slug)
       const media = await this.wp.media().id(post[0].featured_media)
-      const title = post[0].title.rendered
+      const unStrippedTitle = post[0].title.rendered
+      const title = stripHtml(unStrippedTitle)
       const img = media.media_details.sizes['featured-image'].source_url
       const description = post[0].excerpt.rendered
       res.render('postShare', { url: config.HTTP_HOST, frontUrl: config.front_url, description, title, img, category, slug})
